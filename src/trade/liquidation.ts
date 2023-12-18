b76fbc338dab251ec13fc59f0ca2715b71d265ac
Daniel McCloud <daniel@foxify.trade>
2023-12-18 15:02:04 +0000

    :construction: reviewed with Tuan, revert back to OpenPnlFeed address and add additional logs.

--- src/trade/liquidation.ts
+++ src/trade/liquidation.ts
@@ -6,7 +6,7 @@ import {
   getBorrowingFee,
   GetBorrowingFeeContext,
 } from "./fees";
-import { Trade, TradeInfo, TradeInitialAccFees } from "./types";
+import { Trade, TradeInitialAccFees } from "./types";
 
 export type GetLiqPriceContext = GetFundingFeeContext &
   GetRolloverFeeContext &
@@ -38,39 +38,40 @@ export const getLiquidationPrice = (
   );
 
   const fundingFee = getFundingFee(
-  posStable * trade.leverage,
-  initialAccFees.funding,
-  trade.buy,
-  initialAccFees.openedAfterUpdate,
-  {
-    ...context,
-    currentBlock: context.currentL1Block,
-  } as GetFundingFeeContext
-);
+    posStable * trade.leverage,
+    initialAccFees.funding,
+    trade.buy,
+    initialAccFees.openedAfterUpdate,
+    {
+      ...context,
+      currentBlock: context.currentL1Block,
+    } as GetFundingFeeContext
+  );
 
   const liqPriceDistance =
     (trade.openPrice *
-      (posStable * 0.9 -
-        rolloverFee -
-        borrowingFee -
-        fundingFee)) /
+      (posStable * 0.9 - rolloverFee - borrowingFee - fundingFee)) /
     posStable /
     trade.leverage;
 
-    const liqPrice = trade.buy
+  const liqPrice = trade.buy
     ? Math.max(trade.openPrice - liqPriceDistance, 0)
     : Math.max(trade.openPrice + liqPriceDistance, 0);
 
-    console.log(`perpetuals-sdk:liquidation: getLiquidationPrice(): buy=${trade.buy}, 
-      openPrice=${trade.openPrice}, 
-      liqPrice=${liqPrice}, 
+  console.log(`perpetuals-sdk:liquidation: getLiquidationPrice(): buy=${trade.buy},
+      openPrice=${trade.openPrice},
+      liqPrice=${liqPrice},
       liqPriceDistance=${liqPriceDistance},
-      rollingFee=${rolloverFee}, 
-      borrowingFee=${borrowingFee}, 
+      rollingFee=${rolloverFee},
+      borrowingFee=${borrowingFee},
       fundingFee=${fundingFee},
       posStable=${posStable},
       leverage=${trade.leverage}`);
 
-      console.log (`perpetuals-sdk:liquidation: trade=${JSON.stringify(trade)}, context=${JSON.stringify(context)}`)
+  console.log(
+    `perpetuals-sdk:liquidation: trade=${JSON.stringify(
+      trade
+    )}, context=${JSON.stringify(context)}`
+  );
   return liqPrice;
 };
